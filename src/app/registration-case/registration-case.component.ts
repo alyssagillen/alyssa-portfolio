f8f008fc8dc40fe696108e3bc1161abb903151e9
Alyssa Gillen <alyssagillen1234@gmail.com>
2024-01-22 15:00:42 -0600

    Registration case study

--- src/app/registration-case/registration-case.component.ts
+++ src/app/registration-case/registration-case.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-registration-case',
   templateUrl: './registration-case.component.html',
-  styleUrls: ['./registration-case.component.less']
+  styleUrls: ['./registration-case.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegistrationCaseComponent implements OnInit {
 
