5ad9df329483c63785d0cbe5f7b52787bfc40073
xyberstudio <xyberllc@gmail.com>
2024-04-10 12:47:50 -0500

    building

--- src/app/harvestly-case/harvestly-case.component.ts
+++ src/app/harvestly-case/harvestly-case.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-harvestly-case',
   templateUrl: './harvestly-case.component.html',
-  styleUrls: ['./harvestly-case.component.less']
+  styleUrls: ['./harvestly-case.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HarvestlyCaseComponent implements OnInit {
 
